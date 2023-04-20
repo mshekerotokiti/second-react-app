b93f33544019f5100a91c041e118639b7f5088ca
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-04-20 18:08:00 +0300

    Modified

--- src/App.js
+++ src/App.js
@@ -1,33 +1,73 @@
 import React, { useState } from 'react';
-import image from './sharon.jpg';
-import Blog from './components/Blog';
+import image1 from './flowers/Blue-Flowers.jpg';
+import image2 from './flowers/bouquet-roses-17514774.jpg';
+import image3 from './flowers/purple-flower.webp';
 
 function App (){
-const [blog, setBlog] = useState("");
-function blog(info){
-  setBlog(info);
+const [flowers, setFlowers] = useState("");
+
+function getInfo(info){
+  setFlowers(info);
 }
 
     return (
       <div>
-        <img className='center'
-        src= {image}
-        alt="tokiti" />
-        < Header name="My Information" info={blog}/>
-        <h2> What you don't know about me</h2>
-        < Blog />
-
-
-        <>
-          <button onClick={incrementCount}
-          style = {{color: "blue"}}
-          >Click me!</button>
-
-        </>
-
+        <h1>Flowers Edition</h1>
+        <h4>Click on the image to see flower description</h4>
+        <ul className='flower'>
+          <li><BlueFlower name="Cornflower" details={getInfo}/></li>
+          <li><RoseBouquet name="Rose Bouquet" details={getInfo}/></li>
+          <li><PurpleFlower name="Purple flower" details={getInfo}/></li>
+        </ul>
+        <p>{flowers}</p>
       </div>
       
       
+    );
+  }
+
+  function BlueFlower(props){
+     function handleClick(event){
+      event.preventDefault();
+      alert("Cornflowers were mentioned in folklore as a romantic symbol for young men pining over their special someone")
+     }
+     return(
+      <div>
+         <h3>{props.name} </h3>
+        <img src={image1} style= {{width:"50%"}} onClick={handleClick}/>
+      </div>
+     )
+  }
+
+  function RoseBouquet (props) {
+
+    function handleClick(event) {
+      event.preventDefault();   
+      alert("Roses are Red");
+  
+    }
+  
+    return (
+      <div>
+      <h3>{props.name} </h3>
+      <img src={image2}  style= {{width:"50%"}} alt="rose" onClick={handleClick}/>
+      </div>
+    );
+  }
+
+  function PurpleFlower (props) {
+
+    function handleClick(event) {
+      event.preventDefault();   
+      alert("purple flowers ");
+  
+    }
+  
+    return (
+      <div>
+      <h3>{props.name} </h3>
+      <img src={image3} style= {{width:"50%"}}alt="purpleflower" onClick={handleClick}/>
+      </div>
     );
   }
 
